f099dfb7328cc1bb2318ea17479ef674564c074d
YaKsenia <xenia.iakovleva@gmail.com>
2024-05-20 17:29:56 +0200

    updated views

--- russian_propaganda/views.py
+++ russian_propaganda/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from .models import Title
 
 def title_list(request):
-    return render(request, 'propaganda/title_list.html', {})
+    titles = Title.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'propaganda/title_list.html', {'titles': titles})
